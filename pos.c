73d5067ba8ed328740aa0e3399cefae3b61d106a
Xavier-Antony <30952765+Xavier-Antony@users.noreply.github.com>
2017-08-17 17:02:10 +0530

    C Program
    positive / negative

---
+++ pos.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the value:%d\n");
+if(a>0)
+{
+printf("The entered value is positive")
+break;
+}
+if(a<0)
+{
+printf("The entered value is positive")
+break;
+}
+if(a==0)
+{
+printf("The entered value is positive")
+break;
+}
+else
+{
+printf("Invalied")
+}
+getch();
+clrscr;
+}
